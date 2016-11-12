82f467c7b72443eaf6e0580025bdb1c4d8c4bc50
eriksson.e.erik@gmail.com <eriksson.e.erik@gmail.com>
2016-11-12 22:48:17 +0100

    added standard rigidbody controller

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FPSController : MonoBehaviour {
+    Rigidbody body;
 
 	// Use this for initialization
 	void Start () {
-		
+		//body = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
