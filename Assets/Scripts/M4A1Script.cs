6fb3254e4101fea762c2afc05ca490d6761dd9ae
Eric Amundsson <eamundsson@gmail.com>
2016-11-13 21:29:39 +0100

    Found a free M4A1-model in the asset store which I added to the scene as a child to the camera. Also wrote a script for it which uses ray casting to the center of the screen (where the crosshair is). I also began working on a muzzle flash particle system which right now needs some tweaking.

---
+++ Assets/Scripts/M4A1Script.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class M4A1Script : MonoBehaviour {
+
+	public float delayTime=0.2f; //To tweak the rate of fire
+
+	float timer;
+
+	Camera camera;
+	Ray ray;
+	Vector3 centerOfScreen;
+	[SerializeField] GameObject muzzle_flash;
+	[SerializeField] Transform muzzle_flash_spawnPoint;
+
+	void Start () {
+		timer = delayTime; //the gun should be ready at start
+		camera = Camera.main;
+		centerOfScreen = new Vector3 (0.5f, 0.5f, 0);
+		//Make the gun target the middle of the screen (0.5; 0.5) where the crosshair is
+		transform.LookAt (camera.ScreenToViewportPoint (centerOfScreen));
+		transform.RotateAround (transform.position, Vector3.up, 180f); // FIXME: Change the guns rotation before importing it
+	}
+		
+	bool isReady()
+	{
+		if (timer >= delayTime) 
+			return true;
+		return false;
+	}
+
+	void Update () {
+		//ray cast at the middle of the screen
+		ray = camera.ViewportPointToRay (centerOfScreen);
+		//TODO: Add some recoil?
+		RaycastHit hit;
+	
+
+		if (Input.GetMouseButton (0)) {
+			
+			timer+=Time.deltaTime; // make the timer independent of frame rate.
+			Debug.Log (isReady());
+			if (isReady ()) {
+				//spawn the muzzle flash effect (particle system)
+				GameObject flash = (GameObject)Instantiate (muzzle_flash, muzzle_flash_spawnPoint.position, Quaternion.identity);
+				Destroy (flash, 0.2f);
+				if (Physics.Raycast (ray, out hit))
+					Debug.Log (hit.transform.name);
+				timer = 0f; //reset the timer
+			}
+
+		}
+	}
+}
