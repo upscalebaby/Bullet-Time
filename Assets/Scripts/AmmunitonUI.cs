597f95ceb982104e9a3f72107b2e022e8cf81eca
Eric Amundsson <eamundsson@gmail.com>
2016-11-17 06:01:15 +0100

    added ammunition to the m4a1 and a graphical indication of it in the canvas

---
+++ Assets/Scripts/AmmunitonUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(Text))]
+public class AmmunitonUI : MonoBehaviour {
+
+	Text text;
+
+	string maxAmmo;
+	string currentAmmo;
+
+	// Use this for initialization
+	void Start () {
+		if (GameObject.Find ("M4A1") != null) {
+			maxAmmo = GameObject.Find ("M4A1").GetComponent<M4A1Script> ().getAmmoCapacity ().ToString ();
+			currentAmmo = maxAmmo;
+		}
+		text = GetComponent<Text> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentAmmo = GameObject.Find ("M4A1").GetComponent<M4A1Script> ().getCurrentAmmo ().ToString ();
+		text.text = currentAmmo + "/" + maxAmmo;
+	}
+}
--- Assets/Scripts/M4A1Script.cs
+++ Assets/Scripts/M4A1Script.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using UnityEngine;
 
+//TODO: Create a weapon class that this and other weapons can inherit from
 public class M4A1Script : MonoBehaviour {
+
 
 	public float delayTime=0.2f; //To tweak the rate of fire
     public ParticleFactory particleFactory;     // This is not very clean, particleFactory should be globally accessible
@@ -13,9 +15,13 @@ public class M4A1Script : MonoBehaviour {
 	[SerializeField] GameObject muzzle_flash;
     [SerializeField] Transform muzzle_flash_spawnPoint;
     [SerializeField] GameObject collision_particle; // TODO: skapa ett abstraktionslager så inte vapnet behöver hantera detta
+	[SerializeField] int ammunition_capacity=60; //TODO: -||-
+
+	int currentAmmo;
+
 
 	void Start () {
-
+		currentAmmo = ammunition_capacity;
 		timer = delayTime; //the gun should be ready at start
 		centerOfScreen = new Vector3 (0.5f, 0.5f, 0);
 		//Make the gun target the middle of the screen (0.5; 0.5) where the crosshair is
@@ -25,17 +31,31 @@ public class M4A1Script : MonoBehaviour {
 		
 	bool isReady()
 	{
-		if (timer >= delayTime) 
+		if (timer >= delayTime && currentAmmo > 0) 
 			return true;
 		return false;
 	}
 
+	void decrementAmmo(){
+		currentAmmo--;
+	}
+
+	public int getCurrentAmmo(){ //TODO: Maybe not use static methods?
+		return currentAmmo;
+	}
+	public int getAmmoCapacity(){
+		return ammunition_capacity;
+	}
+		
 	void Update () {
 		//ray cast at the middle of the screen
 		ray = Camera.main.ViewportPointToRay (centerOfScreen);
 		//TODO: Add some recoil?
 		RaycastHit hit;
-	
+
+		if (Input.GetKeyDown ("r")) {
+			this.currentAmmo = ammunition_capacity; //TODO: Reload time?
+		}
 
 		if (Input.GetMouseButton (0)) {
 			timer+=Time.deltaTime; // make the timer independent of frame rate.
@@ -43,6 +63,7 @@ public class M4A1Script : MonoBehaviour {
 			if (isReady ()) {
 				//spawn the muzzle flash effect (particle system)
 				GameObject flash = (GameObject)Instantiate (muzzle_flash, muzzle_flash_spawnPoint.position, Quaternion.identity);
+				decrementAmmo ();
 				Destroy (flash, 0.2f);
                 if (Physics.Raycast (ray, out hit)) {
                     Debug.Log (hit.transform.name);
